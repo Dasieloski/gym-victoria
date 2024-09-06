7af9c295ded1c578380107c9029901d1fdffe398
Dasiel <dasieltorres99@gmail.com>
2024-09-06 01:56:59 -0400

    arreglo para desplegar 6

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
-//import { authOptions } from "@/lib/auth";
-
+import { authOptions } from "@/lib/auth"; // Cambiar esta línea para importar authOptions
 
 declare module "next-auth" {
     interface User {
@@ -19,75 +18,6 @@ declare module "next-auth" {
     }
 }
 
-const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "Username" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials: Record<"username" | "password", string> | undefined, req: any) {
-                if (!credentials) return null;
-                const { username, password } = credentials;
-
-                const userFound = await prisma.usuario.findUnique({
-                    where: {
-                        username: credentials.username,
-                    }
-                })
-                if (!userFound) {
-                    throw new Error("El username no está registrado.");
-                }
-                const passwordMatch = await bcrypt.compare(credentials.password, userFound.password)
-                if (!passwordMatch) {
-                    throw new Error("La contraseña es incorrecta.");
-                }
-                return {
-                    id: userFound.id.toString(),
-                    username: userFound.username,
-                    rol: userFound.rol,
-                    nombre: userFound.nombre,
-                    carnetIdentidad: userFound.carnetIdentidad,
-                    telefono: userFound.telefono,
-                };
-
-            }
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id;
-                token.rol = user.rol;
-                token.username = user.username;
-                token.nombre = user.nombre;
-                token.carnetIdentidad = user.carnetIdentidad;
-                token.telefono = user.telefono;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if (session.user) {
-                session.user.rol = token.rol as string;
-                session.user.id = token.id as string;
-                session.user.username = token.username as string;
-                session.user.nombre = token.nombre as string;
-                session.user.carnetIdentidad = token.carnetIdentidad as string;
-                session.user.telefono = token.telefono as string;
-            }
-            return session;
-        }
-    },
-    session: {
-        strategy: "jwt",
-    },
-    pages: {
-        signIn: '/login/inicio',
-        signOut: '/',
-    },
-};
-
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions); // Asegúrate de que esta línea use authOptions importado
 
 export { handler as GET, handler as POST };
