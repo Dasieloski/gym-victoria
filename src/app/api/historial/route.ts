bca2bc34c08f0e4411d0f7164d527bcc981b02b0
Dasiel <dasieltorres99@gmail.com>
2024-09-23 01:22:19 -0400

    arreglando history 4

--- src/app/api/historial/route.ts
+++ src/app/api/historial/route.ts
@@ -6,13 +6,7 @@ export const revalidate = 0;
 
 export async function GET(req: NextRequest) {
   try {
-    const { searchParams } = new URL(req.url);
-    const page = parseInt(searchParams.get('page') || '1', 10);
-    const pageSize = parseInt(searchParams.get('pageSize') || '10', 10);
-
     const historiales = await prisma.historial.findMany({
-      skip: (page - 1) * pageSize,
-      take: pageSize,
       include: {
         usuario: true,
         entrenador: {
@@ -22,19 +16,9 @@ export async function GET(req: NextRequest) {
         },
         membresia: true,
         reserva: true
-      },
-      orderBy: {
-        fecha: 'desc'
       }
     });
-
-    const totalHistoriales = await prisma.historial.count();
-
-    return NextResponse.json({
-      historiales,
-      totalPages: Math.ceil(totalHistoriales / pageSize),
-      currentPage: page
-    }, { status: 200 });
+    return NextResponse.json(historiales, { status: 200 });
   } catch (error) {
     console.error('Error al obtener el historial:', error);
     return NextResponse.json({ error: 'Error al obtener el historial' }, { status: 500 });
