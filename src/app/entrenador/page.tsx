e39c6b1cf480a7709021a6f4cbab006e0a44a87b
Dasiel <dasieltorres99@gmail.com>
2024-09-29 02:53:56 -0400

    cambiando la membresia entrenador 1

--- src/app/entrenador/page.tsx
+++ src/app/entrenador/page.tsx
@@ -364,7 +364,7 @@ export default function TrainerPage() {
                             <Clock className="inline-block mr-1" size={16} />
                             Horarios
                         </button>
-                     {/*    <button
+                        {/*     <button
                             onClick={() => handleTabChange('payments')}
                             className={`px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300 ${activeTab === 'payments' ? 'bg-[#2272FF] text-white' : 'text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700'}`}
                         >
@@ -406,13 +406,13 @@ export default function TrainerPage() {
                         <Clock className="inline-block mr-1" size={16} />
                         Horarios
                     </button>
-                    <button
+                    {/*     <button
                         onClick={() => handleTabChange('payments')}
                         className={`block w-full text-left px-3 py-2 rounded-md text-sm font-medium transition-colors duration-300 ${activeTab === 'payments' ? 'bg-[#2272FF] text-white' : 'text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700'}`}
                     >
                         <DollarSign className="inline-block mr-1" size={16} />
                         Pagos
-                    </button>
+                    </button> */}
                 </div>
             </div>
 
