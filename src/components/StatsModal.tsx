e790f743e6fb570b1448fa28442a538873d9b497
Dasiel <dasieltorres99@gmail.com>
2024-11-22 11:27:32 -0500

    agregar barra de deslizamiento ventana estadisticas 1

--- src/components/StatsModal.tsx
+++ src/components/StatsModal.tsx
@@ -53,7 +53,7 @@
 
      return (
        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-         <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-xl w-11/12 max-w-4xl max-h-90vh overflow-y-auto">
+         <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-xl w-11/12 max-w-4xl max-h-screen overflow-y-auto">
            <h2 className="text-2xl font-bold mb-4 text-gray-800 dark:text-gray-100">Estadísticas de {client.nombre}</h2>
 
            <div className="mb-8">
